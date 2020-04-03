fb2451d5660935b07b1e195fce992588d6c273f1
barkundsupriya-webonise <supriya.barkund@weboniselab.com>
2020-04-03 09:47:04 +0530

    filter commit page added

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -6,27 +6,33 @@ class SiteController < ApplicationController
   def index; end
 
   def list_of_repo
-    @repo_list = @client.org_repos(@client.user.company.downcase,
-                                  {type: "#{params[:type]}"})
+    @repo_list = @client.org_repos(@repo_name, { type: "#{params[:type]}" } )
   end
 
   def list_of_commits
-    @commit_list = @client.list_commits("#{@client.user.company.downcase}/#{params[:project]}",
-                    options: { author: "#{params[:author] }"})
+      @commit_list = @client.list_commits("#{@repo_name}/#{params[:project]}",
+                      query: { author: "#{params[:author]}"})
   end
 
   def list_of_branches
-    @branch_list = @client.branches("#{@client.user.company.downcase}/#{params[:project]}")
+    @branch_list = @client.branches("#{@repo_name}/#{params[:project]}")
   end
 
   def list_of_pulls
-    @pull_list = @client.pulls("#{@client.user.company.downcase}/#{params[:project]}",
+    @pull_list = @client.pulls("#{@repo_name}/#{params[:project]}",
                                 state: "#{params[:state]}")
+  end
+
+  def filter_commits
+    @filter_commits = @client.list_commits("#{@repo_name}/#{params[:project]}",
+                    query: { author: "#{params[:author]}"})
   end
 
   private
 
   def get_api
-    @client = Octokit::Client.new(login: 'barkundsupriya-webonise', password: 'webonise123#')
+    @client = Octokit::Client.new(login: 'barkundsupriya-webonise',
+                                  password: 'webonise123#')
+    @repo_name = @client.user.company.downcase
   end
 end
